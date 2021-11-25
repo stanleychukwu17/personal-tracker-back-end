194c3d74e0bb0942db4f540b96f5325694dd2dbd
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-11-25 06:50:47 +0100

    changes

--- app.js
+++ app.js
@@ -26,16 +26,43 @@ app.get('/get-the-goals/', async (req, res) => {
 
 // saves the goals archived for a particular day
 app.post('/save-this-archive/', async (req, res) => {
-    // const connection = await mysql.createConnection(dbObject);
     const {theDay, theMonth, theYear, goals} = req.body;
     if (theDay < 10) { theDay = `0${theDay}`; }
     if (theMonth < 10) { theMonth = `0${theMonth}`; }
 
+    let typ_val = '', typ_hours = 0, hour_val, mins_val;
+
     // format the date and get it ready for our mysql database
     const date_fmt = `${theYear}-${theMonth}-${theDay}`
+    const connection = await mysql.createConnection(dbObject);
 
     goals.forEach( async (ech) => {
-        console.log(ech, 'with me')
+        ech.typ_hours = 0; ech.typ_val = typ_val = '';
+
+        if (ech.typ == 'select_yes' && ech.val && ech.val.length > 0) { ech.typ_val = typ_val = ech.val }
+        else if (ech.typ == 'select_yes' && !ech.val) { ech.typ_val = typ_val = ech.def }
+
+        if (ech.typ == 'select_time' || ech.typ == 'input_hours') {
+            hour_val = (ech.hour_val && Number(ech.hour_val)) || 0
+            mins_val = (ech.mins_val && Number(ech.mins_val)) || 0
+            if (mins_val > 0) {
+                mins_val = mins_val/60
+                hour_val = hour_val + mins_val;
+            }
+
+            ech.typ_hours = typ_hours = hour_val
+        }
+    
+        let [rows] = await connection.execute(`SELECT * from goals_completed where date_w = '${date_fmt}' and typ_id = ${ech.id} limit 1`);
+        if (rows[0]) {
+            console.log(ech.id, rows, `SELECT * from goals_completed where date_w = '${date_fmt}' and typ_id = ${ech.id} limit 1`)
+        } else {
+            let [result] = await connection.execute(`INSERT INTO goals_completed (date_w, typ_id, typ, typ_val, typ_hours) values
+                ('${date_fmt}', ${ech.id}, '${ech.typ}', '${ech.typ_val}', ${ech.typ_hours})`);
+            // let {insertId} = result; or let insertId = result.insertId
+            // console.log(`INSERT INTO goals_completed (date_w, typ_id, typ, typ_val, typ_hours) values ('${date_fmt}', ${ech.id}, '${ech.typ}', '${ech.typ_val}', ${ech.typ_hours})`)
+        }
     })
+
     res.json({'msg':'okay', 'cause':'Moving higher!'})
 })
