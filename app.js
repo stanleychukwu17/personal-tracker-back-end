bea00c2010285dba3f820f4ea4158309ea5e936c
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-12-17 15:31:23 +0100

    changes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const monthNames = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug"
 const app = express();
 app.listen(4000, () => { console.log('listening on port ', 4000) })
 
+
 // Config of Middlewares
 app.use(cors({origin: '*'}))
 app.use(express.static('/public'));
