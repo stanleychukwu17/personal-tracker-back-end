10bad0ba996cba15ecedcf486d442651a5b77114
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-08 06:07:48 +0100

    changes

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const dayArr = [
 const fullMonthNames = [
     "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
 ];
-const monthNames = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const monthNames = [
+    "Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
 
 // creates the node-express app
 const app = express();
